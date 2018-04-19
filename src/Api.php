4ffdb55e9706100dd99a8b785639a3ad323fad98
Camille Baronnet <c.baronnet@cpro.fr>
2018-04-19 10:00:00 +0200

    Create API wrapper

--- src/Api.php
+++ src/Api.php
@@ -4,4 +4,118 @@ namespace Starif\ApiWrapper;
 
 class Api
 {
+    /**
+     * @var TransportInterface
+     */
+    private $transport;
+
+    public function __construct(TransportInterface $transport)
+    {
+        $this->transport = $transport;
+    }
+
+    public function getTransport()
+    {
+        return $this->transport;
+    }
+
+    public function getClients(array $filters = []): array
+    {
+        return $this->transport->request('/clients', $filters);
+    }
+
+    public function getClient(int $id): array
+    {
+        return $this->transport->request('/client/'.$id);
+    }
+
+    public function updateClient(int $id, $attributes): array
+    {
+        return $this->transport->request('/client/'.$id, $attributes, 'put');
+    }
+
+    public function createClient($attributes): array
+    {
+        return $this->transport->request('/client', $attributes, 'post');
+    }
+
+    public function deleteClient(int $id): array
+    {
+        return $this->transport->request('/client/'.$id, [], 'delete');
+    }
+
+    public function getCatalogues(array $filters = []): array
+    {
+        return $this->transport->request('/catalogues', $filters);
+    }
+
+    public function getCatalogue(int $id): array
+    {
+        return $this->transport->request('/catalogue/'.$id);
+    }
+
+    public function updateCatalogue(int $id, $attributes): array
+    {
+        return $this->transport->request('/catalogue/'.$id, $attributes, 'put');
+    }
+
+    public function createCatalogue($attributes): array
+    {
+        return $this->transport->request('/catalogue', $attributes, 'post');
+    }
+
+    public function deleteCatalogue(int $id): array
+    {
+        return $this->transport->request('/catalogue/'.$id, [], 'delete');
+    }
+
+    public function getTarifs(array $filters = []): array
+    {
+        return $this->transport->request('/tarifs', $filters);
+    }
+
+    public function getTarif(int $id): array
+    {
+        return $this->transport->request('/tarif/'.$id);
+    }
+
+    public function updateTarif(int $id, $attributes): array
+    {
+        return $this->transport->request('/tarif/'.$id, $attributes, 'put');
+    }
+
+    public function createTarif($attributes): array
+    {
+        return $this->transport->request('/tarif', $attributes, 'post');
+    }
+
+    public function deleteTarif(int $id): array
+    {
+        return $this->transport->request('/tarif/'.$id, [], 'delete');
+    }
+
+    public function getMateriels(array $filters = []): array
+    {
+        return $this->transport->request('/materiels', $filters);
+    }
+
+    public function getMateriel(int $id): array
+    {
+        return $this->transport->request('/materiel/'.$id);
+    }
+
+    public function updateMateriel(int $id, $attributes): array
+    {
+        return $this->transport->request('/materiel/'.$id, $attributes, 'put');
+    }
+
+    public function createMateriel($attributes): array
+    {
+        return $this->transport->request('/materiel', $attributes, 'post');
+    }
+
+    public function deleteMateriel(int $id): array
+    {
+        return $this->transport->request('/materiel/'.$id, [],'delete');
+    }
 }
---
+++ tests/ApiTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Starif\ApiWrapper\Tests;
+
+use Mockery;
+use Starif\ApiWrapper\TransportInterface;
+use PHPUnit\Framework\TestCase;
+use Starif\ApiWrapper\Api;
+
+class ApiTest extends TestCase
+{
+    const ENDPOINTS = ['client', 'catalogue', 'materiel', 'tarif'];
+    protected $token;
+    protected $entrypoint;
+
+    const WITH_FILTER = ['with_filters'];
+    const WITHOUT_FILTER = ['without_filters'];
+    const ID_ENTITY = 123;
+
+    protected function createFakeTransport()
+    {
+        $transport = Mockery::mock(TransportInterface::class);
+        $transport->shouldReceive('request')->withArgs([Mockery::any(), self::WITH_FILTER])->andReturn(self::WITH_FILTER);
+        $transport->shouldReceive('request')->withArgs([Mockery::any(), []])->andReturn(self::WITHOUT_FILTER);
+        $transport->shouldReceive('request')->withArgs([Mockery::pattern('#/'.self::ID_ENTITY.'$#')])->andReturn(['entity']);
+        return $transport;
+    }
+
+    public function setUp()
+    {
+        $this->token = 'token_jwt';
+        $this->entrypoint = 'https://exemple/api/';
+    }
+
+    public function testApiWithoutTransport()
+    {
+        $this->expectException(\TypeError::class);
+        new Api(null);
+    }
+
+    public function testApiWithTransport()
+    {
+        $transport = Mockery::mock(TransportInterface::class);
+        $api = new Api($transport);
+        $this->assertInstanceOf(TransportInterface::class, $api->getTransport());
+    }
+
+    public function testGetWithoutFilters()
+    {
+        $transport = $this->createFakeTransport();
+        $api = new Api($transport);
+        foreach(self::ENDPOINTS as $endpoint){
+            $this->assertEquals(self::WITHOUT_FILTER, $api->{'get'.ucfirst($endpoint).'s'}());
+        }
+    }
+
+    public function testGetWithFilters()
+    {
+        $transport = $this->createFakeTransport();
+        $api = new Api($transport);
+        foreach(self::ENDPOINTS as $endpoint){
+            $this->assertEquals(self::WITH_FILTER, $api->{'get'.ucfirst($endpoint).'s'}(self::WITH_FILTER));
+        }
+    }
+
+    public function testTryToGetSpecificEntityWithoutIdAsArgument()
+    {
+        $this->expectException(\TypeError::class);
+        $transport = $this->createFakeTransport();
+        $api = new Api($transport);
+        foreach(self::ENDPOINTS as $endpoint){
+            $api->{'get'.ucfirst($endpoint)}();
+        }
+    }
+
+    public function testTryToGetSpecificEntity()
+    {
+        $transport = $this->createFakeTransport();
+        $api = new Api($transport);
+        foreach(self::ENDPOINTS as $endpoint){
+            $entity = $api->{'get'.ucfirst($endpoint)}(self::ID_ENTITY);
+            $this->assertInternalType('array', $entity);
+        }
+    }
+}
