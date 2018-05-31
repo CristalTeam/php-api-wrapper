2094ccee227e23192f88702f193ea93a13ccbd4e
Camille Baronnet <c.baronnet@cpro.fr>
2018-05-31 15:37:42 +0200

    Fix setAttribute

--- src/Concerns/HasAttributes.php
+++ src/Concerns/HasAttributes.php
@@ -56,6 +56,17 @@ trait HasAttributes
     }
 
     /**
+     * Determine if a set mutator exists for an attribute.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function hasSetMutator($key)
+    {
+        return method_exists($this, 'set'.self::studly($key).'Attribute');
+    }
+
+    /**
      * Set a given attribute on the model.
      *
      * @param  string $key
@@ -64,6 +75,15 @@ trait HasAttributes
      */
     public function setAttribute($key, $value)
     {
+        // First we will check for the presence of a mutator for the set operation
+        // which simply lets the developers tweak the attribute as it is set on
+        // the model, such as "json_encoding" an listing of data for storage.
+        if ($this->hasSetMutator($key)) {
+            $method = 'set'.self::studly($key).'Attribute';
+
+            return $this->{$method}($value);
+        }
+
         // If an attribute is listed as a "date", we'll convert it from a DateTime
         // instance into a form proper for storage on the database tables using
         // the connection grammar's date format. We will auto set the values.
