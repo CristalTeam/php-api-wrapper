4ee86c3273b0e102f315940bd921dbd6028b4233
Florent FOUCHIER <f.fouchier@cpro.fr>
2018-09-04 10:19:18 +0200

    Get ne doit pas renvoyer de 404

--- src/Builder.php
+++ src/Builder.php
@@ -228,11 +228,11 @@ class Builder
     public function get()
     {
         $instance = $this->getModel();
-        try {
-            $entities = $instance->getApi()->{'get'.ucfirst($instance->getEntity()).'s'}($this->getQuery());
-        } catch (ApiEntityNotFoundException $e) {
-            return [];
-        }
+        // try {
+        $entities = $instance->getApi()->{'get'.ucfirst($instance->getEntity()).'s'}($this->getQuery());
+        // } catch (ApiEntityNotFoundException $e) {
+        // return [];
+        // }
 
         return $this->instanciateModels($entities);
     }
